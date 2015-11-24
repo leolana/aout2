cca1b09bceccad84ff0d0a0770533f646e7cb29d
Leonardo Lana <leolucas.lana@hotmail.com>
2015-11-24 01:36:41 -0200

    Add new solution for exercises from book "The art of unit testing". First deployed examples of the second chapter.

---
+++ ArtOfUnitTesting2ndEd.Exercises/ArtOfUnitTesting2ndEd.Exercises/LogAnalyzer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtOfUnitTesting2ndEd.Exercises
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName) => fileName.EndsWith(".SLF");
+    }
+}
---
+++ ArtOfUnitTesting2ndEd.Exercises/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,19 @@
+﻿using ArtOfUnitTesting2ndEd.Exercises;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidFileName_BadExtension_ReturnsFalse()
+        {
+            var analyzer = new LogAnalyzer();
+
+            var result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+
+            Assert.False(result);
+        }
+    }
+}
