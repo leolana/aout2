011d23ccf2343dac15cb9ded96f8780ed7ba30f0
Leonardo Lana <leolucas.lana@hotmail.com>
2015-12-08 00:57:41 -0200

    Add unit tests using NUnit parameterized tests .

---
+++ ArtOfUnitTesting2ndEd.Exercises/LogAn.UnitTests/LogAnalyzerTestsParameterizedTests.cs
@@ -0,0 +1,21 @@
+﻿using ArtOfUnitTesting2ndEd.Exercises;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTestsParameterizedTests
+    {
+        [TestCase("filewithbadextension.foo", false)]
+        [TestCase("filewithbadextension.slf", true)]
+        [TestCase("filewithbadextension.SLF", true)]
+        public void IsValidFileName_ValidExtensions_ReturnsTrue(string file, bool expected)
+        {
+            var analyzer = new LogAnalyzer();
+
+            var result = analyzer.IsValidLogFileName(file);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
