a2c57551c6ce19f681f0a8acb9791e0e2813ba0b
Leonardo Lana <leolucas.lana@hotmail.com>
2015-12-08 00:58:13 -0200

    Using SetUp and TearDown configuration unit tests.

---
+++ ArtOfUnitTesting2ndEd.Exercises/LogAn.UnitTests/LogAnalyzerTestsWithSetupTearDown.cs
@@ -0,0 +1,39 @@
+﻿using ArtOfUnitTesting2ndEd.Exercises;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTestsWithSetUpTearDown
+    {
+        private LogAnalyzer analyzer = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            analyzer = new LogAnalyzer();
+        }
+
+        [Test]
+        public void IsValidFileName_ValidFileLowerCase_ReturnsTrue()
+        {
+            var result = analyzer.IsValidLogFileName("filewithbadextension.slf");
+
+            Assert.IsTrue(result, "filename should be valid!");
+        }
+
+        [Test]
+        public void IsValidFileName_ValidFileUpperCase_ReturnsTrue()
+        {
+            var result = analyzer.IsValidLogFileName("filewithbadextension.SLF");
+
+            Assert.IsTrue(result, "filename should be valid!");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            analyzer = null; //Don't do it in real life.
+        }
+    }
+}
