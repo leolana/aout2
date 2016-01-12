32b3e6a223af950acb89c90f052a0774061f2bdf
leolana <leolucas.lana@hotmail.com>
2016-01-12 09:12:02 -0200

    [Chapter 2]: Add under test method Sum of Calculator.

---
+++ ArtOfUnitTesting2ndEd.Exercises/LogAn.UnitTests/MemCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArtOfUnitTesting2ndEd.Exercises;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    public class MemCalculatorTests
+    {
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            var calc = new MemCalculator();
+
+            var lastSum = calc.Sum();
+
+            Assert.AreEqual(0, lastSum);
+        }
+    }
+}
