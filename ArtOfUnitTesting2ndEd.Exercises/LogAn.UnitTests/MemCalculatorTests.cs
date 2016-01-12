95d1069ab9c904d77a4e16c1a6391b3cc8423dac
leolana <leolucas.lana@hotmail.com>
2016-01-12 09:20:31 -0200

    [Chapter 2]: Add test for check method Add and Sum.

--- ArtOfUnitTesting2ndEd.Exercises/LogAn.UnitTests/MemCalculatorTests.cs
+++ ArtOfUnitTesting2ndEd.Exercises/LogAn.UnitTests/MemCalculatorTests.cs
@@ -20,6 +20,17 @@ namespace LogAn.UnitTests
             Assert.AreEqual(0, lastSum);
         }
 
+        [Test]
+        public void Add_WhenCalled_ChangesSum()
+        {
+            var calc = MakeCalc();
+
+            calc.Add(1);
+            var sum = calc.Sum();
+
+            Assert.AreEqual(1, sum);
+        }
+
         private static MemCalculator MakeCalc()
         {
             return new MemCalculator();   
